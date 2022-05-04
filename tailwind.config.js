7ca0423ed7ea805263a2a2a152497ed26221520f
thijsBoet <m.boet@chello.nl>
2022-05-04 17:23:14 +0200

    final commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 module.exports = {
-  purge: [],
+  purge: {
+    content: ["./src/**/*.jsx", "./index.html"],
+    enabled: process.env.NODE_ENV === "production",
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          light: "#3fbaeb",
+          DEFAULT: "#0fa9e6",
+          dark: "#0c87b8",
+        }
+      },
+      fontFamily: {
+        headline: 'Poppins, sans-serif',
+      },
+    },
   },
   variants: {
     extend: {
